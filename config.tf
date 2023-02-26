6e518ff20e8a34ee47abc09587288fb82c7bd572
David Hill <david@davidhill.ie>
2023-02-26 23:09:47 +0000

    Configure bucket to call lambda function

--- config.tf
+++ config.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_s3_bucket" "bucket"{
+resource "aws_s3_bucket" "bucket" {
   bucket = "vidhill-my-tf-test-bucket"
 
   tags = {
@@ -11,8 +11,36 @@ resource "aws_s3_bucket" "bucket"{
   }
 }
 
-resource "aws_s3_bucket" "bucket1"{
-  bucket =  "${aws_s3_bucket.bucket.bucket}-resized"
+resource "aws_s3_bucket" "bucket1" {
+  bucket = "${aws_s3_bucket.bucket.bucket}-resized"
+}
+
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.test_lambda.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.bucket.arn
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_acl" "example1" {
+  bucket = aws_s3_bucket.bucket1.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.test_lambda.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+
 }
 
 data "aws_iam_policy_document" "example1" {
@@ -34,8 +62,8 @@ data "aws_iam_policy_document" "example1" {
     ]
 
     resources = [
-      "arn:aws:s3:::vidhill-my-tf-test-bucket/*",
-      "arn:aws:s3:::vidhill-my-tf-test-bucket-resized/*"
+      "${aws_s3_bucket.bucket.arn}/*",
+      "${aws_s3_bucket.bucket1.arn}/*"
     ]
   }
 }
@@ -68,6 +96,11 @@ resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
 
   assume_role_policy = data.aws_iam_policy_document.example2.json
+}
+
+resource "aws_iam_role_policy_attachment" "basic" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.iam_for_lambda.id
 }
 
 resource "aws_lambda_function" "test_lambda" {
